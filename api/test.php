761ff14760729380737f976e353fff8244fd9e60
Yash Gohel <yashgohel16@gmail.com>
2020-07-04 22:13:39 +0530

    Update test.php

--- api/test.php
+++ api/test.php
@@ -5,7 +5,7 @@
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $link,
     CURLOPT_SSL_VERIFYHOST => 0,
-    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_FOLLOWLOCATION => false,
     CURLOPT_USERAGENT => 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36',
     CURLOPT_AUTOREFERER => false,
     CURLOPT_HEADER => false
