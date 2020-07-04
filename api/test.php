39f6de0553eb6e93d6327de563b1f2b0fc4cb896
Yash Gohel <yashgohel16@gmail.com>
2020-07-04 22:11:32 +0530

    Update test.php

--- api/test.php
+++ api/test.php
@@ -4,7 +4,11 @@
   curl_setopt_array($curl, [
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $link,
-    CURLOPT_USERAGENT => 'IIG curl request'
+    CURLOPT_SSL_VERIFYHOST => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_USERAGENT => 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36',
+    CURLOPT_AUTOREFERER => false,
+    CURLOPT_HEADER => false
   ]);
 
   $resp = curl_exec($curl);
