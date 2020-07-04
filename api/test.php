57ff85391d6a5b2e69ca7feff30a5a9cc31a9fdf
Yash Gohel <yashgohel16@gmail.com>
2020-07-05 00:07:00 +0530

    Update test.php

--- api/test.php
+++ api/test.php
@@ -5,7 +5,7 @@
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $link,
     CURLOPT_HTTPHEADER => [
-        'Cookie: _ga=GA1.1.1858824391.1587317770; hblid=BiC4bmXs9ruxVjST3m39N0Ja4bAdAab6; _ga_PQ59P2BKSP=GS1.1.1593718474.48.1.1593719199.0'
+        'Cookie: ig_did=E24C5C9D-8F14-4CF1-97F2-44E17142A296; mid=XuCEBQALAAGpwPd_k6kSnxtPfe6D; shbid=17634; datr=b-LhXgsHC3KiBWZw-a7U0PJ7; shbts=1593630989.5520513; csrftoken=EMI26nJLlrhavUwJKW50mWxM2ETzaK3y; ds_user_id=5698133721; sessionid=5698133721%3AaNQiLMvWGHrzDc%3A22; rur=PRN; urlgen="{\"2405:205:c84a:4db:a8f1:6f6d:1232:bc56\": 55836\054 \"2405:205:c84a:4db:94ae:69c0:d4a:ce92\": 55836}:1jrn04:jWExUEgCr37-xbCFImYteSmCwiY"',
     ],
     CURLOPT_SSL_VERIFYHOST => 0,
     CURLOPT_FOLLOWLOCATION => false,
@@ -14,6 +14,7 @@
   ]);
 
   $resp = curl_exec($curl);
+  curl_close($curl);
 
   echo $resp;
 ?>
