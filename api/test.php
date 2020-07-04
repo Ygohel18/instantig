c637ecc0a53c9fee12e93871163139fc32147775
Yash Gohel <yashgohel16@gmail.com>
2020-07-04 23:25:30 +0530

    Update test.php

--- api/test.php
+++ api/test.php
@@ -1,6 +1,6 @@
 <?php 
   $curl = curl_init();
-  $link = 'https://www.instagram.com/p/CCOCs6jAFn4/?__a=1&csrf=EMI26nJLlrhavUwJKW50mWxM2ETzaK3y';
+  $link = 'https://www.instagram.com/p/CCOCs6jAFn4/?__a=1';
   curl_setopt_array($curl, [
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $link,
@@ -8,7 +8,8 @@
     CURLOPT_FOLLOWLOCATION => false,
     CURLOPT_USERAGENT => 'Instagram 15.206.133.54 Android (23/6.0.1; 320dpi; 720x1280; samsung; SM-A310F; a3xelte; samsungexynos7580; en_EN; 5698133721)',
     CURLOPT_HEADER => false,
-    CURLOPT_HTTP_VERSION => 84
+    CURLOPT_HTTP_VERSION => 84,
+    CURLOPT_COOKIE => 'csrftoken=EMI26nJLlrhavUwJKW50mWxM2ETzaK3y;ds_user_id=5698133721;mid=XuCEBQALAAGpwPd_k6kSnxtPfe6D;sessionid=5698133721%3AaNQiLMvWGHrzDc%3A22'
 
   ]);
 
