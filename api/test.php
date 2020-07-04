81510cc853dfa0ccd3a632ff296a57319bb34cdb
Yash Gohel <yashgohel16@gmail.com>
2020-07-04 22:00:24 +0530

    Update test.php

--- api/test.php
+++ api/test.php
@@ -3,7 +3,7 @@
   $link = 'https://www.instagram.com/p/CCOCs6jAFn4/?__a=1';
   curl_setopt_array($curl, [
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => $link.'?__a=1',
+    CURLOPT_URL => $link,
     CURLOPT_USERAGENT => 'IIG curl request'
   ]);
 
