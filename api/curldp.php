55ff53ebf15a0e1bfafe46e6c984356e1bcfb20f
Yash Gohel <yashgohel16@gmail.com>
2020-07-12 11:26:10 +0530

    Update curldp.php

--- api/curldp.php
+++ api/curldp.php
@@ -11,7 +11,7 @@ if (isset($_POST["username"])) {
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_URL => 'https://www.instagram.com/'.$link.'/?__a=1',
         CURLOPT_HTTPHEADER => [
-            'Cookie: ig_did=E24C5C9D-8F14-4CF1-97F2-44E17142A296; mid=XuCEBQALAAGpwPd_k6kSnxtPfe6D; shbid=17634; datr=b-LhXgsHC3KiBWZw-a7U0PJ7; shbts=1593630989.5520513; csrftoken=EMI26nJLlrhavUwJKW50mWxM2ETzaK3y; ds_user_id=5698133721; sessionid=5698133721%3AaNQiLMvWGHrzDc%3A22; rur=PRN; urlgen="{\"2405:205:c84a:4db:a8f1:6f6d:1232:bc56\": 55836\054 \"2405:205:c84a:4db:94ae:69c0:d4a:ce92\": 55836}:1jrn04:jWExUEgCr37-xbCFImYteSmCwiY"',
+            'Cookie: ig_did=A03A5753-4EC9-415D-A778-1B3925B31B09; mid=XwqlXQALAAFJfrVx5-YFYwN6q-uH; csrftoken=dhGmbuTSSxnZGZRCk7PtyRdAu7gjojUl; ds_user_id=8173227811; sessionid=8173227811%3AGa3hy7FFSMwSzt%3A15; shbid=17634; shbts=1594533239.137384; rur=PRN; urlgen="{\"2409:4041:2e96:97d:41ea:c89e:8f03:e3b9\": 55836}:1juUwN:i_ct7JlDKyPf_4BXHKfwPO4NKsc"',
         ],
         CURLOPT_SSL_VERIFYHOST => 0,
         CURLOPT_FOLLOWLOCATION => false,
